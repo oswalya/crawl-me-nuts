229a959f8561cbfb630b0d3cbf84eb56423781f7
Bjoern Jessen-Noak <bjess@esailors.de>
2017-03-30 10:18:21 +0200

    add argument parsing, simplify code

--- crawler.py
+++ crawler.py
@@ -7,6 +7,7 @@ import requests
 import re
 import inspect
 import logging
+import argparse
 
 class Product:
     def __init__(self, asin, name, price_new, price_used, link):
@@ -26,11 +27,19 @@ class Product:
     def toJson(self):
         return json.dumps({'asin': self.asin, 'name': self.name, 'price_new': self.new, 'price_used': self.used, 'price_diff': str(self.getDiff()), 'link': self.link})
 
+class Xpathdef:
+    _NEXT_PAGE = '//a[@id="pagnNextLink"]'
+    _PRODUCT = '//li[contains(@id,"result_")]'
+    _NAME = './/a[@class="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]'
+    _PRICE_NEW = './/span[@class="a-size-base a-color-price s-price a-text-bold"]/text()'
+    _PRICE_USED = './/span[@class="a-size-base a-color-price a-text-bold"]/text()'
+
+
+global MAX_PAGE_COUNT;
+global MIN_PERCENT_SAVING
 
 def getProductDetailsPage(url):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-
-    XPATH_NEXT_PAGE = '//a[@id="pagnNextLink"]'
     COUNTER = 1
 
     while True:
@@ -40,21 +49,16 @@ def getProductDetailsPage(url):
         page = requests.get(mod_url,headers=headers)
         doc = html.fromstring(page.content)
         readProductDetails(doc)
-        if not doc.xpath(XPATH_NEXT_PAGE):
+        if (not doc.xpath(Xpathdef._NEXT_PAGE)) or (COUNTER > MAX_PAGE_COUNT):
             break
 
 
 def readProductDetails(document):
-    XPATH_PRODUCT = '//li[contains(@id,"result_")]'
-    XPATH_NAME = './/a[@class="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]'
-    XPATH_PRICE_NEW = './/span[@class="a-size-base a-color-price s-price a-text-bold"]/text()'
-    XPATH_PRICE_USED = './/span[@class="a-size-base a-color-price a-text-bold"]/text()'
-
-    for i in document.xpath(XPATH_PRODUCT):
+    for i in document.xpath(Xpathdef._PRODUCT):
         # print(html.tostring(i, pretty_print=True))
-        RAW_NAME = i.xpath(XPATH_NAME)
-        RAW_NEW_PRICE = i.xpath(XPATH_PRICE_NEW)
-        RAW_USED_PRICE = i.xpath(XPATH_PRICE_USED)
+        RAW_NAME = i.xpath(Xpathdef._NAME)
+        RAW_NEW_PRICE = i.xpath(Xpathdef._PRICE_NEW)
+        RAW_USED_PRICE = i.xpath(Xpathdef._PRICE_USED)
         RAW_ASIN = i.get('data-asin')
 
         NAME = RAW_NAME[0].get('title').encode('utf-8') if RAW_NAME else None
@@ -67,9 +71,8 @@ def readProductDetails(document):
 
 def saveItem(product):
     if not isinstance(product, Product):
-        logging.debug()
         raise ValueError('Object must be an instance of Product!')
-    if product.getDiff() and product.getDiff() > 30.0:
+    if product.getDiff() and product.getDiff() > MIN_PERCENT_SAVING:
         logging.info(product.toJson())
 
 def xstr(s):
@@ -81,11 +84,32 @@ def run(filename):
     with open(filename) as f:
         content = f.readlines()
     for line in content:
+        s = line.strip()
+        if s == "":
+            logging.debug('empty line, skipping....')
+            continue
+        if s.startswith('#'):
+            logging.debug('commented out, skipping...')
+            continue
         logging.info(line.strip() + "\n------------------------------------------------------\n")
         getProductDetailsPage(line.strip())
 
 
 if __name__ == "__main__":
     # Run('/home/bjess/workspace/python/amazon_crawler/pages.txt')
-    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-    run('pages.txt')
+    parser = argparse.ArgumentParser(description='Get crawling boundaries')
+    parser.add_argument('-f', '--file', help='the file containing all urls to crawl. Defaults to: pages.txt', default='pages.txt')
+    parser.add_argument('-s', '--saving', help='minimal percentage saving (default 30.0)', default=30.0, type=Decimal)
+    parser.add_argument('-p', '--pages', help='Max pages to search (default 50)', default=50, type=int)
+    parser.add_argument('-v', '--verbose', help='enable more verbose output', action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    else:
+        logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+    MIN_PERCENT_SAVING = args.saving
+    MAX_PAGE_COUNT = args.pages
+
+    run(args.file)
